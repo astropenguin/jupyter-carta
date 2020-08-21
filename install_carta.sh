5fab65bf62676b33537ed4fca44f5a866dab877a
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-22 00:53:22 +0900

    Update install_carta.sh

--- install_carta.sh
+++ install_carta.sh
@@ -1,11 +1,15 @@
-echo 'Install CARTA ...'
+echo -n 'Install CARTA ... '
+
 wget -q https://github.com/CARTAvis/carta/releases/download/v1.3.1/CARTA-v1.3.1-remote.tgz
 tar xf CARTA-v1.3.1-remote.tgz
 rm CARTA-v1.3.1-remote.tgz
 mv CARTA-v1.3.1-remote carta
 rm carta/carta-backend/lib/libstdc++.so.6
 
-echo 'Install ngrok ...'
+echo 'done!'
+
+echo -n 'Install ngrok ... '
+
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -q ngrok-stable-linux-amd64.zip
 rm ngrok-stable-linux-amd64.zip
