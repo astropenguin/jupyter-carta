f25ebab436c77115dad634ffb3d0ca746a751738
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-21 23:59:08 +0900

    Add install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,12 @@
+echo 'Install CARTA ...'
+wget -q https://github.com/CARTAvis/carta/releases/download/v1.3.1/CARTA-v1.3.1-remote.tgz
+tar xf CARTA-v1.3.1-remote.tgz
+mv CARTA-v1.3.1-remote carta
+rm carta/carta-backend/lib/libstdc++.so.6
+
+echo 'Install ngrok ...'
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip -q ngrok-stable-linux-amd64.zip
+mv ngrok carta/ngrok
+
+echo 'Done!'
